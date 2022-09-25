98ce38b76dce9a94ddb5311920a90261ab66328f
Julio Litzenberg <jltzbrg@gmail.com>
2022-09-26 01:01:48 +0200

    Creating Movie

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
-export default function Movie({ name, genre, year }) {
+export default function Movie({ name, genre, year, image }) {
   return (
     <div className="card">
       <div className="container">
+        <img
+          src={image}
+          alt="Harry Potter Movie"
+          width="100%"
+          height="230px"
+        />
         <h2>{name}</h2>
         <h4>
           {genre} - {year}
