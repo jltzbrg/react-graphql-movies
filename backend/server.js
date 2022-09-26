74a2971e5f76485a7f086df6604d408760a15080
Julio Litzenberg <jltzbrg@gmail.com>
2022-09-26 10:57:39 +0200

    Create mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,25 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
+const mongoose = require('mongoose');
+
+// Create Mongo Connection
+mongoose.connect(
+  `${process.env.MONGO_URI}`,
+  {
+    useNewUrlParser: true,
+  },
+  (error) => {
+    if (error) {
+      console.log(
+        `Unable to connect to database! --> ${error.message}`
+      );
+    } else {
+      console.log('Database successful connected.');
+    }
+  }
+);
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
